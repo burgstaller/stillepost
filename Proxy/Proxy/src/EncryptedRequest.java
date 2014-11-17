e40f95d95831e7e1ea7b1d57cdd8b23aefda00c5
Florian Burgstaller <florian.burgstaller@student.tugraz.at>
2014-11-17 14:41:15 +0100

    proxy stub project (intelij)

---
+++ Proxy/Proxy/src/EncryptedRequest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by florian on 17.11.14.
+ */
+public class EncryptedRequest {
+    private byte[] data;
+}
---
+++ Proxy/Proxy/src/PlainRequest.java
@@ -0,0 +1,7 @@
+/**
+ * Created by florian on 17.11.14.
+ */
+public class PlainRequest extends EncryptedRequest{
+    private String port;
+    private String hostName;
+}
