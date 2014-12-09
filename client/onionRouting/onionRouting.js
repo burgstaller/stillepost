2dba09bda385596251fc84ac649075501a5efff9
Andreas Derler <andreas.derler@gmail.com>
2014-12-09 22:03:40 +0100

    First draft of build onion layers

--- client/onionRouting/onionRouting.js
+++ client/onionRouting/onionRouting.js
@@ -1,47 +1,178 @@
-sp.onion = (function() {
-    var public = {},
+window.stillepost = window.stillepost || {};
+window.stillepost.onion = (function() {
+  var public = {},
+    cu = window.stillepost.cryptoUtils,
+    webrtc = window.stillepost.webrtc,
+    chainSize = 3,
 
-    //Id of the chain in which this node is the master
-    chainIdMaster:null,
+    //todo: remove static node list - only for test purposes
+    nodeList = [],
 
-    //sym keys of all nodes of the chain in which this node is the master
-    masterChain:null,
+  //Id of the chain in which this node is the master
+  chainIdMaster = null,
 
-    //map of all node-neighbours with socket and key info
-    chainMap:null,
+  //sym keys of all nodes of the chain in which this node is the master
+  masterChain = null,
 
-    //generate RSA keypair and send public key to directory server
-    initOnionSetup: function(){
+  //map of all node-neighbours with socket and key info
+  chainMap = null,
 
-    },
-    
-    //requests list of nodes, creates 'create' request and sends it to the first node in the chain (waits for ack from exit node)
-    createChain: function(){
+  //generate RSA keypair and send public key to directory server
+  initOnionSetup = function(){
+    cu.getGeneratedPublicKey().then(function(pubKey) {
+      // todo: send pubKey to server
+      console.log("Generated pubKey: "+pubKey);
+    }).catch(function(err) {
+      console.log("Error generating public RSA Key", err);
+    });
+  },
 
-    	//TODO request and choose nodes
+  //requests list of nodes from server and chooses nodes
+  retrieveNodes = function() {
+    //TODO request and choose nodes
 
-    	//generate new sym keys
-
-    	//encrypt build command n-1 times
-
-
-    },
-
-    //is called when node is intermediate to add another node to the current (to be created) chain
-    addNodeToChain: function(){
-
-    },
-
-    //interface function to generically send a new message over the master chain
-    public.sendMessage = function(){
-
+    //todo: remove dummy objects - only for testing purposes
+    var keyPairPromises = [];
+    for(var i = 0; i < chainSize; i++) {
+      keyPairPromises.push(cu.getGeneratedRSAKeyPair());
     }
+    return Promise.all(keyPairPromises).then(function(keyPairs) {
+      console.log('keyPairs: ', keyPairs);
+      var nodes = [];
+      for (var i = 0; i< keyPairs.length; i++) {
+        nodes[i] = {};
+        nodes[i].privKey = keyPairs[i].privateKey;
+        nodes[i].pubKey = JSON.parse(keyPairs[i].publicKey);
+        nodes[i].socket = {peer: "127.0.0.1", port:1337};
+      }
+      return nodes;
+    });
+  },
 
+  createLayer = function(key, pubKey, data, pubKeyNextNode, socket) {
+    return cu.wrapAESKey(key, pubKey).then(function(keyData) {
+      console.log("keyData: " + keyData);
+      var iv = cu.generateNonce(),
+        dataToEncrypt = null;
 
-    //interface function called by WEBRtc to handle an incoming onion request
-    public.handleMessage = function(){
+      if (pubKeyNextNode) {
+        // create intermediate node layer
+        dataToEncrypt = JSON.stringify({nodeSocket: socket, nodePubKey: pubKeyNextNode, iv: iv, data: data});
+      } else {
+        // create exit node layer
+        dataToEncrypt = JSON.stringify({iv: iv, data: data});
+      }
+      console.log("encrypting data: "+dataToEncrypt);
+      return cu.encryptAES(dataToEncrypt, key, iv).then(function(encData) {
+        return {keyData: keyData, chainData:encData, iv:iv};
+      });
+    });
+  },
 
+  //requests list of nodes, creates 'create' request and sends it to the first node in the chain (waits for ack from exit node)
+  createChain = function(){
+
+    return retrieveNodes().then(function(nodes) {
+      console.log('nodes: ', nodes);
+      nodeList = nodes;
+      //generate new sym keys
+      return cu.getGeneratedAESKeys(chainSize).then(function(keys) {
+        console.log("AES-keys: ", keys);
+        masterChain = keys;
+
+        //encrypt build command n-1 times
+
+        // build onion layers of chain build information
+        // {commandName: 'build', keyData:E_pn1(sym_key1),
+        //      chainData:E_sym_key1(E_pn2(sym_key2) || n2Socket || n2Pub || E_sym_key2(E_pn3(sym_key) || E_pn3(..same same)))}
+
+        // todo: encrypt iv?
+        // todo: why does node1 need pubKey of node2? (same with node2 - node3)
+
+        // build innermost layer - exit node
+        var dataExitNode = "ensure that this data is the same size as the chainData for other sockets";
+        return createLayer(keys[0], nodes[0].pubKey, dataExitNode).then(function(layerDataExit) {
+          // build second innermost layer - node2
+          // This is the encrypted Exit Node Data which is again encrypted
+          var intermediateData = {keyData: layerDataExit.keyData, chainData: layerDataExit.chainData, iv: layerDataExit.iv};
+          return createLayer(keys[1], nodes[1].pubKey, intermediateData, nodes[0].pubKey, nodes[0].socket).then(function(layerDataIntermediate) {
+
+            // build third layer - entry node
+            var entryNodeData = {keyData: layerDataIntermediate.keyData, chainData: layerDataIntermediate.chainData, iv: layerDataIntermediate.iv};
+            return createLayer(keys[2], nodes[2].pubKey, entryNodeData, nodes[1].pubKey, nodes[1].socket).then(function(layerDataEntry) {
+              var command = {commandName: 'build', keyData: layerDataEntry.keyData, chainData: layerDataEntry.chainData, iv: layerDataEntry.iv};
+              console.log("created command: ", command);
+
+              //todo:remove this line - only for testing purposes - we fake retrieving message from other node in order to validate encryption
+              public.handleMessage(command);
+
+              //todo: send via webrtc and wait for response
+              //var con = new webrtc.createConnection(nodes[0].socket.peer, nodes[0].socket.port);
+              //con.send(command);
+            });
+          });
+        }, function (err) {
+          console.log("Error while building layers", err);
+        });
+      }, function(err) {
+        console.log("Error while generating AES keys",err);
+      });
+    }, function(err) {
+      console.log("Error while retrieving nodes",err);
+    });
+  },
+
+  //is called when node is intermediate to add another node to the current (to be created) chain
+  addNodeToChain = function(){
+
+  };
+
+  //interface function to generically send a new message over the master chain
+  public.sendMessage = function(message) {
+    // init chain
+    if (!masterChain) {
+      createChain().then(function () {
+        console.log("created Chain");
+      }).catch(function(err) {
+        //todo error handling
+        console.log("error creating chain: ",err);
+      });
+    } else {
+      // send message over chain
     }
+  };
 
+  //interface function called by WEBRtc to handle an incoming onion request
+  public.handleMessage = function(message) {
+    console.log("Handle message: ",message);
+    if (message.commandName === 'build') {
+      // handle build chain command
+      console.log("Handle build message - going to decrypt");
 
-    })();
+      // Todo: Since we fake being a node while testing - we use test function instead of cu.decryptAES
+      cu.testDecryptAES(message.chainData, message.keyData, message.iv, nodeList[2].privKey).then(function(decData) {
+        var decryptedJson = JSON.parse(decData);
+        console.log("Decrypted data: ", decryptedJson);
+        if (decryptedJson.nodeSocket) {
+          // send to next socket
+          console.log("Sending build command to next node: ",decryptedJson.nodeSocket);
+        } else {
+          // exit node logic - return "OK"-message?
+
+        }
+      }).catch(function(err) {
+        // todo: handle error
+        console.log("Error decrypting data",err);
+      });
+
+    } else if (message.commandName === 'ajaxRequest') {
+      // handle AJAX request
+    } else {
+      // handle invalid message commandName
+    }
+  };
+
+  initOnionSetup();
+
+  return public;
+})();
--- client/onionRouting/utils/cryptoUtils.js
+++ client/onionRouting/utils/cryptoUtils.js
@@ -1,73 +1,160 @@
-var para = {name: "AES-GCM", length: 256};
-var pubExp = new Uint8Array([1, 0, 1]);
-var rsa = {name: "RSA-OAEP", modulusLength: 2048, publicExponent: pubExp, hash: {name: "SHA-256"}};
-var keyFormat = "jwk";
-var privateRSAKey = null;
+window.stillepost = window.stillepost || {};
 
-crypto.subtle.generateKey(rsa,true,["wrapKey", "unwrapKey"]).then(function(keyPair) {
-	console.log("rsa-pubkey: "+keyPair.publicKey);
-	crypto.subtle.exportKey(keyFormat,keyPair.publicKey).then(function(exportKey) {
-		console.log("exported rsa pubkey: "+exportKey);
-		console.log("exported rsa pubkey decoded: "+JSON.stringify(exportKey));
+window.stillepost.cryptoUtils = (function() {
+	var public = {};
 
-		wrapGeneratedAESKey(exportKey);
-	}, function(err){console.log(err);});
-	console.log("rsa-privkey: "+keyPair.privateKey);
-	privateRSAKey = keyPair.privateKey;
-},function(err) {
-	console.log(err);
-});
+	var aesAlgorithm = {name: "AES-GCM", length: 256};
+	var pubExp = new Uint8Array([1, 0, 1]);
+	var rsaAlgorithm = {name: "RSA-OAEP", modulusLength: 4096, publicExponent: pubExp, hash: {name: "SHA-256"}};
+	var keyFormat = "jwk";
+	var privateRSAKey = null;
 
-function wrapGeneratedAESKey(keyEncryptionKey) {
-	crypto.subtle.generateKey(para, true,["encrypt", "decrypt"]).then(
-		function(key) {
-			console.log("AES-key"+key);
+	/**
+	 * Generates a nonce
+	 * @returns Promise which evaluates to a 128 bit nonce of type Uint8Array(16)
+	 */
+	public.generateNonce = function() {
+		return crypto.getRandomValues(new Uint8Array(16));
+	};
 
-			crypto.subtle.importKey(keyFormat, keyEncryptionKey, rsa, false, ["wrapKey"]).then(function(keyEncKey) {
-				console.log("imported keyEncKey: "+keyEncKey);
-				crypto.subtle.wrapKey(keyFormat, key, keyEncKey,rsa).then(function(wrappedKey) {
-					console.log("wrappedKey: "+wrappedKey);
-					var decodedWrappedKey = ab2str(wrappedKey);
-					console.log("decoded wrappedKey: "+decodedWrappedKey);
-					unwrapKey(decodedWrappedKey);
-				}, function(err){console.log(err);});
-			}, function(err){console.log(err);});
-
-		}
-	);
-}
-
-function unwrapKey(decodedWrappedKey) {
-	var wrappedKey = str2ab(decodedWrappedKey);
-	console.log("re-encoded wrappedKey: "+wrappedKey);
-	crypto.subtle.unwrapKey(keyFormat, wrappedKey,privateRSAKey, rsa, para, false,["encrypt","decrypt"]).then(function(key) {
-		var nonce = crypto.getRandomValues(new Uint8Array(16));
-		console.log(nonce);
-
-		var alg = {name: "AES-GCM", iv: nonce};
-		var buffer = new TextEncoder("utf-8").encode("secretText");
-
-		crypto.subtle.encrypt(alg,key,buffer).then(function(enc) {
-				console.log('enc'+enc);
-				console.log('dec '+new TextDecoder("utf-8").decode(new Uint8Array(enc)));
-				crypto.subtle.decrypt(alg,key,enc).then(function(dec) {
-					console.log('dec '+new TextDecoder("utf-8").decode(new Uint8Array(dec)));
+	/**
+	 * Locally generates a RSA-OAEP key pair of 4096 modulus length.
+	 * The private key is stored in a local member.
+	 * The public rsa key is wrapped and exported.
+	 * @returns Promise which evaluates to the string representation of the exported publicKey
+	 */
+	public.getGeneratedPublicKey = function() {
+		// todo: do we need a random public exponent? ie.:
+		// rsaAlgorithm.publicExponent = <random pubExp>
+		return crypto.subtle.generateKey(rsaAlgorithm, true, ["wrapKey", "unwrapKey"]).then(function (keyPair) {
+				privateRSAKey = keyPair.privateKey;
+				return crypto.subtle.exportKey(keyFormat, keyPair.publicKey).then(function(exportKey) {
+					// todo: if we use random pubExp we need to return it and store it on the server ie:
+					// return {publicKey: JSON.stringify(exportKey), publicExponent:pubExp};
+					return JSON.stringify(exportKey);
 				});
-			}
-		);
-	}, function(err){console.log(err);});
+			});
+	};
 
+	// todo: remove if no longer required: function only used for testing purposes - used to simulate public keys retrieved from directory server
+	public.getGeneratedRSAKeyPair = function() {
+		return crypto.subtle.generateKey(rsaAlgorithm, true, ["wrapKey", "unwrapKey"]).then(function (keyPair) {
+			return crypto.subtle.exportKey(keyFormat, keyPair.publicKey).then(function(exportKey) {
+				return {publicKey: JSON.stringify(exportKey), privateKey: keyPair.privateKey};
+			});
+		});
+	};
 
-}
+	/**
+	 * Generates a list of AES-GCM keys of 256 bit length, which can be used for encryption and decryption.
+	 * @param count the amount of AES keys to be generated
+	 * @returns Promise which evaluates to the list of generated AES keys
+	 */
+	public.getGeneratedAESKeys = function(count) {
+		var promises = [];
+		for(var i = 0; i < count; i++) {
+			promises.push(generateAESKey());
+		}
+		return Promise.all(promises);
+	};
 
-function ab2str(buf) {
-	return String.fromCharCode.apply(null, new Uint16Array(buf));
-}
-function str2ab(str) {
-	var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-	var bufView = new Uint16Array(buf);
-	for (var i=0, strLen=str.length; i < strLen; i++) {
-		bufView[i] = str.charCodeAt(i);
+	/**
+	 * Generates a single AES-GCM key of 256 bit length, which can be used for encryption and decryption.
+	 * @returns Promise which evaluates to AES key
+	 */
+	function generateAESKey() {
+		return crypto.subtle.generateKey(aesAlgorithm, true, ["encrypt", "decrypt"]).then(function(key) {
+			return key;
+		});
 	}
-	return buf;
-}
+
+	/**
+	 * Wraps an AES key with a RSA keyEncryptionKey.
+	 * @param key the key which is to be wrapped
+	 * @param keyEncryptionKey the exported keyEncryptionKey which is used to wrap the key
+	 * @returns Promise which evaluates to the string represenation of the wrapped AES key
+	 */
+	public.wrapAESKey = function(key, keyEncryptionKey) {
+		// Since given key is an exported key we need to import it first
+		return crypto.subtle.importKey(keyFormat, keyEncryptionKey, rsaAlgorithm, false, ["wrapKey"]).then(function(keyEncKey) {
+			return crypto.subtle.wrapKey(keyFormat, key, keyEncKey, rsaAlgorithm).then(function(wrappedKey) {
+				return ab2str(wrappedKey);
+			});
+		});
+	};
+
+	public.encryptWrappedAES = function(data, wrappedKey, iv) {
+		return unwrapAESKey(wrappedKey).then(function(unwrappedKey) {
+			return public.encryptAES(data, unwrappedKey, iv);
+		});
+	};
+
+	public.encryptAES = function(data, key, iv) {
+		var alg = {name: "AES-GCM", iv: iv};
+		var buffer = str2ab(data);
+		return crypto.subtle.encrypt(alg, key, buffer).then(function(encData) {
+			return ab2str(encData);
+		});
+	};
+
+	public.decryptAES = function(encData, key, iv) {
+		var alg = {name: "AES-GCM", iv: iv};
+		var buffer = str2ab(encData);
+		return unwrapAESKey(key).then(function(unwrappedKey) {
+			return crypto.subtle.decrypt(alg, unwrappedKey, buffer).then(function(decData) {
+				return ab2str(decData);
+			});
+		});
+	};
+
+	function unwrapAESKey(wrappedKey) {
+		// Since wrapped key is string we first need to parse it to a ArrayBuffer object
+		var wrappedKeyAB = str2ab(wrappedKey);
+		return crypto.subtle.unwrapKey(keyFormat, wrappedKeyAB, privateRSAKey, rsaAlgorithm, aesAlgorithm, false,["encrypt","decrypt"]);
+	}
+
+	/**
+	 * Converts ArrayBuffer object to string object
+	 * @param buf the ArrayBuffer object
+	 * @returns {string} the stringified ArrayBuffer object
+	 */
+	function ab2str(buf) {
+		return String.fromCharCode.apply(null, new Uint16Array(buf));
+	}
+
+	/**
+	 * Converts string object to ArrayBuffer object
+	 * @param str the string to be converted to type ArrayBuffer
+	 * @returns {ArrayBuffer} the ArrayBuffer object
+	 */
+	function str2ab(str) {
+		var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+		var bufView = new Uint16Array(buf);
+		for (var i=0, strLen=str.length; i < strLen; i++) {
+			bufView[i] = str.charCodeAt(i);
+		}
+		return buf;
+	}
+
+	// TODO: IMPORTANT - Remove following function - only used for test purposes
+	// Decrypt with given private key - Since we fake being a node - we need to pass privkey
+	public.testDecryptAES = function(encData, key, iv, privKey) {
+		var alg = {name: "AES-GCM", iv: iv};
+		var buffer = str2ab(encData);
+		return unwrapAESKey(key, privKey).then(function(unwrappedKey) {
+			return crypto.subtle.decrypt(alg, unwrappedKey, buffer).then(function(decData) {
+				return ab2str(decData);
+			});
+		});
+	};
+
+	//Same as above - we need to pass privateKey in order to fake being a node
+	function unwrapAESKey(wrappedKey, privKey) {
+		// Since wrapped key is string we first need to parse it to a ArrayBuffer object
+		var wrappedKeyAB = str2ab(wrappedKey);
+		return crypto.subtle.unwrapKey(keyFormat, wrappedKeyAB, privKey, rsaAlgorithm, aesAlgorithm, false,["encrypt","decrypt"]);
+	}
+
+
+	return public;
+})();
