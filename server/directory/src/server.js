e691313a14f85128a03abec0223c61e3465e3895
Florian Burgstaller <florian.burgstaller@student.tugraz.at>
2014-12-11 19:27:21 +0100

    added cors to dir server

--- server/directory/src/server.js
+++ server/directory/src/server.js
@@ -12,12 +12,17 @@ server.use(restify.bodyParser());
 var nodes = [];
 
 server.get('/getList', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.send(JSON.stringify(nodes));
+    next();
 });
 server.get('/sendHeardBeat', function (req, res, next) {
     // TODO
 });
 server.post('/register', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     var body  = JSON.parse(req.body);
     nodes.push({ip:body.address, port:body.port, key:body.key});
     // TODO send ack
