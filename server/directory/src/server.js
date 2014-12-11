af736a6546050372a61ee05a32ca99b7679ed200
Gabriel Schanner <gabriel.schanner@student.tugraz.at>
2014-12-11 22:05:51 +0100

    added unique uid for nodes, reworked data management in general, added more error cases

--- server/directory/src/server.js
+++ server/directory/src/server.js
@@ -1,4 +1,5 @@
-var restify = require('restify');
+var restify = require('restify'),
+    uuid = require('uuid');
 
 // Server
 var server = restify.createServer({
@@ -9,30 +10,94 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-var nodes = [];
+var nodes = {},
+    nodesArray = [],
+    idRegistry = {};
 
-server.get('/getList', function (req, res, next) {
+
+// adds a new node to both the map and array of nodes
+// if a entry was already present for a given socket, we need to update
+nodes.addEntry = function (entry) {
+    if (typeof(nodes[entry.socket]) !== "undefined") {
+        for (var i = 0; i < nodesArray.length; i++) {
+            if (nodesArray[i].socket === entry.socket)
+                nodesArray[i] = entry;
+        }
+    }
+    else {
+        nodesArray.push(entry);
+    }
+    nodes[entry.socket] = entry;
+    idRegistry[entry.id] = entry.socket;
+};
+
+nodes.deleteEntry = function (socket) {
+    delete idRegistry[nodes[socket].id];
+    delete nodes[socket];
+
+    for (var i = 0; i < nodesArray.length; i++) {
+        if (nodesArray[i].socket === socket) {
+            nodesArray.splice(i, 1);
+            return true;
+        }
+    }
+    return false;
+};
+
+server.get('/nodelist:id', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.send(JSON.stringify(nodes));
+
+    var id = req.params.id;
+    if(typeof(id) === "undefined" || typeof(idRegistry[id]) === "undefined")
+        res.send(400, createResponseObject('Id is missing or wrong'));
+
+    res.send(200, createResponseObject("OK", nodesArray));
     next();
 });
-server.get('/sendHeardBeat', function (req, res, next) {
-    // TODO
+server.get('/heartbeat:id', function (req, res, next) {
+    var id = req.params.id;
+    if(typeof(id) === "undefined" || typeof(idRegistry[id]) === "undefined")
+        res.send(400, createResponseObject('Id is missing or wrong'));
+    res.send(200, createResponseObject('OK'));
+    return next();
 });
 server.post('/register', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    var body  = JSON.parse(req.body);
-    nodes.push({ip:body.address, port:body.port, key:body.key});
-    // TODO send ack
-    //res.send();
+    var body = JSON.parse(req.body);
+    if (typeof(body.address) === "undefined" || typeof(body.port) === "undefined" || typeof(body.key) === "undefined") {
+        res.send(400, createResponseObject('Required fields not present(address, port and key)'));
+    }
+    var genId = uuid.v4();
+    nodes.addEntry({ip: body.address, port: body.port, socket: body.address + ":" + body.port, key: body.key, lastBeat: new Date().getTime(), id:genId});
+    res.send(200, createResponseObject('OK', genId));
     return next();
 });
-server.get('/logout', function (req, res, next) {
-    // TODO
+server.post('/logout', function (req, res, next) {
+    var body = JSON.parse(req.body),
+        id = body.id,
+        socket = body.socket;
+
+    if(typeof(id) === "undefined" || typeof(idRegistry[id]) === "undefined")
+        res.send(400, createResponseObject('Id is missing or wrong'));
+    if(typeof(nodes[socket]) === "undefined")
+        res.send(400, createResponseObject('No such socket registered'+socket+" "+req.query+" "+req.query.length));
+    if (nodes.deleteEntry(socket))
+        res.send(200, createResponseObject('OK'));
+    else
+        res.send(400, createResponseObject('Not registered. (Perhaps multiple logout calls)'));
+    return next();
 });
 
-server.listen(80, function () {
+server.listen(42111, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
+
+function createResponseObject(message, dataObject) {
+    if (typeof(message) === "undefined")
+        return "\n";
+    if (typeof(dataObject) === "undefined")
+        return {msg: message};
+    return {msg: message, data: dataObject};
+}
